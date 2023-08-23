f6c40816d7c99838b102d82d5c336831d2a9f7ff
jakoub47 <jakoubmcil@gmail.com>
2023-08-23 10:35:54 +0100

    add state to cart

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
-
+import {useState} from 'react'
 function App() {
+  const [cartIsShow,setCartIsShow]=useState(false)
+  const showCartHandler=()=>{setCartIsShow(true)}
+  const closeCartHandler=()=>{setCartIsShow(false)}
+
   return (
     <CartProvider>
-      <Header />
+      {cartIsShow && <p>cart is loading....</p>}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
